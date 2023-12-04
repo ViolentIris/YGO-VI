1a065870bc21ed2032d38b5a9320c38bdef8ad88
VI-1911 <2571845421@qq.com>
2023-12-04 22:09:36 +0800

    .

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -1901,7 +1901,7 @@ void Game::OnResize() {
 		btnCRpos.LowerRightCorner.X - 860,
 		btnCRpos.LowerRightCorner.Y - 400,
 		btnCRpos.LowerRightCorner.X - 580,
-		btnCRpos.LowerRightCorner.Y - 63));
+		btnCRpos.LowerRightCorner.Y - 43));
 	wRM->setRelativePosition(recti(
 		btnCRpos.LowerRightCorner.X - 530,
 		btnCRpos.LowerRightCorner.Y - 455,
