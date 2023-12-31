d7f939a6f09239913a26571ac8a20b4ce54ab3b6
VI-1911 <2571845421@qq.com>
2023-12-31 23:42:19 +0800

    .

--- gframe/single_duel.cpp
+++ gframe/single_duel.cpp
@@ -351,6 +351,8 @@ void SingleDuel::StartDuel(DuelPlayer* dp) {
 	hand_result[1] = 0;
 	players[0]->state = CTOS_HAND_RESULT;
 	players[1]->state = CTOS_HAND_RESULT;
+	players[0]->player_id = 0;
+	players[1]->player_id = 1;
 	duel_stage = DUEL_STAGE_FINGER;
 }
 void SingleDuel::HandResult(DuelPlayer* dp, unsigned char res) {
@@ -398,8 +400,6 @@ void SingleDuel::TPResult(DuelPlayer* dp, unsigned char tp) {
 		return;
 	duel_stage = DUEL_STAGE_DUELING;
 	bool swapped = false;
-	pplayer[0] = players[0];
-	pplayer[1] = players[1];
 	if((tp && dp->type == 1) || (!tp && dp->type == 0)) {
 		DuelPlayer* p = players[0];
 		players[0] = players[1];
@@ -540,15 +540,6 @@ void SingleDuel::DuelEndProc() {
 				NetServer::ReSendToPlayer(*oit);
 			duel_stage = DUEL_STAGE_END;
 		} else {
-			if(players[0] != pplayer[0]) {
-				players[0] = pplayer[0];
-				players[1] = pplayer[1];
-				players[0]->type = 0;
-				players[1]->type = 1;
-				Deck d = pdeck[0];
-				pdeck[0] = pdeck[1];
-				pdeck[1] = d;
-			}
 			ready[0] = false;
 			ready[1] = false;
 			players[0]->state = CTOS_UPDATE_DECK;
@@ -562,7 +553,7 @@ void SingleDuel::DuelEndProc() {
 	}
 }
 void SingleDuel::Surrender(DuelPlayer* dp) {
-	if(dp->type > 1 || !pduel)
+	if (dp->type > 1 || dp->player_id > 1 || !pduel)
 		return;
 	unsigned char wbuf[3];
 	uint32 player = dp->type;
@@ -573,13 +564,9 @@ void SingleDuel::Surrender(DuelPlayer* dp) {
 	NetServer::ReSendToPlayer(players[1]);
 	for(auto oit = observers.begin(); oit != observers.end(); ++oit)
 		NetServer::ReSendToPlayer(*oit);
-	if(players[player] == pplayer[player]) {
-		match_result[duel_count++] = 1 - player;
-		tp_player = player;
-	} else {
-		match_result[duel_count++] = player;
-		tp_player = 1 - player;
-	}
+	match_result[duel_count] = 1 - dp->player_id;
+	++duel_count;
+	tp_player = player;
 	EndDuel();
 	DuelEndProc();
 	event_del(etimer);
@@ -638,15 +625,14 @@ int SingleDuel::Analyze(unsigned char* msgbuffer, unsigned int len) {
 			for(auto oit = observers.begin(); oit != observers.end(); ++oit)
 				NetServer::ReSendToPlayer(*oit);
 			if(player > 1) {
-				match_result[duel_count++] = 2;
+				match_result[duel_count] = 2;
 				tp_player = 1 - tp_player;
-			} else if(players[player] == pplayer[player]) {
-				match_result[duel_count++] = player;
-				tp_player = 1 - player;
-			} else {
-				match_result[duel_count++] = 1 - player;
-				tp_player = player;
 			}
+			else {
+				match_result[duel_count] = players[player]->player_id;
+				tp_player = 1 - player;
+			}
+			++duel_count;
 			EndDuel();
 			return 2;
 		}
@@ -975,7 +961,7 @@ int SingleDuel::Analyze(unsigned char* msgbuffer, unsigned int len) {
 			NetServer::SendBufferToPlayer(players[1 - cc], STOC_GAME_MSG, offset, pbuf - offset);
 			for(auto oit = observers.begin(); oit != observers.end(); ++oit)
 				NetServer::ReSendToPlayer(*oit);
-			if (cl != 0 && (cl & 0x80) == 0 && (cl != pl || pc != cc))
+			if (cl != 0 && (cl & LOCATION_OVERLAY) == 0 && (cl != pl || pc != cc))
 				RefreshSingle(cc, cl, cs);
 			break;
 		}
