0a689147b3dc9654a84dbea3600643e08d382658
VI-1911 <2571845421@qq.com>
2023-12-31 22:35:31 +0800

    .

--- gframe/game.h
+++ gframe/game.h
@@ -897,6 +897,18 @@ extern Game* mainGame;
 #define TEXTURE_HEAD_S				0
 
 #define DEFAULT_DUEL_RULE			5
+//STOC_GAME_MSG messages
+#define MSG_WAITING				3
+#define MSG_START				4
+#define MSG_UPDATE_DATA			6	// flag=0: clear
+#define MSG_UPDATE_CARD			7	// flag=QUERY_CODE, code=0: clear
+#define MSG_REQUEST_DECK		8
+#define MSG_REFRESH_DECK		34
+#define MSG_CARD_SELECTED		80
+#define MSG_UNEQUIP				95
+#define MSG_BE_CHAIN_TARGET		121
+#define MSG_CREATE_RELATION		122
+#define MSG_RELEASE_RELATION	123
 
 #define CARD_ARTWORK_VERSIONS_OFFSET	10
 #define MAX_LAYER_COUNT	6
