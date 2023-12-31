d6ee48e83db4c7dac01ba9a23dc5389219bf2744
VI-1911 <2571845421@qq.com>
2024-01-01 00:02:59 +0800

    .

--- gframe/duelclient.cpp
+++ gframe/duelclient.cpp
@@ -34,6 +34,7 @@ mt19937 DuelClient::rnd;
 bool DuelClient::is_refreshing = false;
 int DuelClient::match_kill = 0;
 std::vector<HostPacket> DuelClient::hosts;
+std::vector<std::wstring> DuelClient::hosts_srvpro;
 std::set<std::pair<unsigned int, unsigned short>> DuelClient::remotes;
 event* DuelClient::resp_event = 0;
 unsigned int DuelClient::temp_ip = 0;
